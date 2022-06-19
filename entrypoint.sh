221818e308647f70abcf66cdff25e38f8235689f
wentokay <wentokay@users.noreply.github.com>
2022-06-18 20:46:34 -0400

    remove xvfb-run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -l
 
+Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
+
 export DISPLAY=:99.0
 export PUPPETEER_EXEC_PATH="chromium"
 
 solana-test-validator > /dev/null 2>&1 &
 echo "solana-test-validator started"
 
-# https://github.com/peterdanis/electron-puppeteer-demo/issues/171
-cmd="xvfb-run $1"
+cmd=$1
 echo "Running '$cmd'"
 if $cmd; then
     echo "Successfully ran '$cmd'"
